b0f692d15cc118a21d1a7851d4c901f450594dba
Coder rudra <83690766+xxdev-specs@users.noreply.github.com>
2021-08-06 16:48:16 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+```python
+# follow replit.com/@ilikeeggz so you can understand better
+# so first it will take a user input
+
+input("click enter for a joke!")
+print("this is the joke")
+
+# the process repeats
+# please follow follow me for more how things work
+```
